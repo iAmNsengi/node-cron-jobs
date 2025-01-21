f7971624ca975fd216e8efb024245e86efd6c9a2
iAmNsengi <iamnsengi@icloud.com>
2025-01-21 18:07:03 +0200

    fx: server init

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 5001;
+
+const app = express();
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log("Server running on port: ", PORT);
+});
